6e6ce5153c9b1d8d4acc0522b36fa74247241dab
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-16 11:15:56 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -23,7 +23,8 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square value = {i} />;
+    return <Square 
+        value = {i} />;
   }
 
   render() {
