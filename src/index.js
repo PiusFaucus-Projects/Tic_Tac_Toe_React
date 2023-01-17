b152ac6ce48429090284efdfaa0afbe8b50cf54f
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-17 09:35:42 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
