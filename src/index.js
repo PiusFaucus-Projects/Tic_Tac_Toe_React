eb24df1b1703f01faa431c9b0ee2b6447ff210b7
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-17 10:01:49 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -59,7 +59,9 @@ class Game extends React.Component {
     }
     squares[i] =this.state.xIsNext ? 'X' : 'O';
     this.setState({
-      history: history.concat([])
+      history: history.concat([{
+        
+      }])
       squares: squares,
       xIsNext: !this.state.xIsNext,
       });
