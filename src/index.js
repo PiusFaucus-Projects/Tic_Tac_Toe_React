aa785f81d2a850b9cdc2c07767f5e6d255d95c27
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-17 10:26:19 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -65,22 +65,6 @@ class Game extends React.Component {
     );
   }
   
-  handleClick(i) {
-    const history = this.state.history;
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([{
-        squares: squares,
-      }]),
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-  
 }
 
 // ========================================
