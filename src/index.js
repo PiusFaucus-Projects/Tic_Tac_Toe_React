547dfbcb020363788ee5d3ed6361a389a0275fbb
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-17 09:21:02 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+function Square(params) {
+  
+} Square extends React.Component {
   render() {
     return (
       <button 
