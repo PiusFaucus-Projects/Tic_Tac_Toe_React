d2aba188633055acea6eea16eaf6a105da9d0c12
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-17 09:59:46 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -48,6 +48,17 @@ class Board extends React.Component {
 
 class Game extends React.Component {
   render() {
+    handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+    squares[i] =this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+      });
+  }
     const history = this.state.history;
     const current = history[history.length -1];
     const winner = calculateWinner(current.squares);
