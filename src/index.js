173c86caba89ea16821438bd71eadd8d170837d8
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-19 10:28:45 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ class Game extends React.Component {
         <li key={move}>
           <button onClick ={() => this.jumpTo(move)}>{desc}</button>
       );
-    })
+    });
  
     let status;
     if (winner) {
