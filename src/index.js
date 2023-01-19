38ce59185f820eede9c539ff00e2ae111fca2f49
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-19 09:47:19 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -37,6 +37,7 @@ class Board extends React.Component {
   render() {
     return (
       <div>
+        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
