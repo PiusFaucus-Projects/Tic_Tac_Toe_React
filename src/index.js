42bfdf67310ba9fffbbaa0164af08fe47d20b13c
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-19 10:08:08 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -74,22 +74,22 @@ class Game extends React.Component {
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-                                                                                                                                                                                                              
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board
-              squares={current.squares}
-              onClick={(i) => this.handleClick(i)}
-            />
-          </div>
-          <div className="game-info">
-              <div className="status">{status}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
+ 
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            onClick={(i) => this.handleClick(i)}
+          />
         </div>
-      );
-    }
+        <div className="game-info">
+          <div className="status">{status}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 // ========================================
 
