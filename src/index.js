2731d53ccc949eee50ca747017fdbd28338d3461
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-19 10:28:35 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -82,8 +82,8 @@ class Game extends React.Component {
       const desc = move ? 'Go to move #' + move : 'Go to game start';
       return (
         <li key={move}>
-          <button onClick ={() => this.jumpTo(move)}>{desc}</button
-      )
+          <button onClick ={() => this.jumpTo(move)}>{desc}</button>
+      );
     })
  
     let status;
