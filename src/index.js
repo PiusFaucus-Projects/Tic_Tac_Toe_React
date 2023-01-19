97e69e758bf7591ec6e282ec715b2e7db97d8e9f
candidate-171781 <candidate-171781@candidate.hackajob.co>
2023-01-19 10:29:16 +0000

    auto-commit-agent

--- src/index.js
+++ src/index.js
@@ -79,10 +79,13 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) => {
-      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
       return (
         <li key={move}>
-          <button onClick ={() => this.jumpTo(move)}>{desc}</button>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
       );
     });
  
